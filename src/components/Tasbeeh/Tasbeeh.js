6712f9736428fc474dcc65d31ea416e3e198e81b
Ali Raza <aliraza.dev.web@gmail.com>
2022-12-05 16:34:25 +0500

    shared-preference-task

--- src/components/Tasbeeh/Tasbeeh.js
+++ src/components/Tasbeeh/Tasbeeh.js
@@ -1,16 +1,22 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 
 const Tasbeeh = () => {
   const [count, setCount] = useState(0);
 
-  const tasbeehCounting = () => {
-    setCount(count + 1)
-  }
+  useEffect(() => {
+    const data = window.localStorage.getItem('tasbeeh-id');
+    if (data !== null) setCount(JSON.parse(data))
+  }, [])
+
+  useEffect(() => {
+    window.localStorage.setItem('tasbeeh-id', JSON.stringify(count))
+  }, [count])
 
   return (
     <div className='tasbeehCount'>
-    <button className='add' onClick={() => tasbeehCounting()}>+</button>
-      <span className=''>{count}</span>
+      <button className='add' onClick={() => setCount(count + 1)}>+</button>
+        <span className=''>{count}</span>
+      <button className='add' onClick={() => setCount(0)}>Reset</button>
     </div>
   )
 }
